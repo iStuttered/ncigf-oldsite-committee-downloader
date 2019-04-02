ed1fcce510a61c50d899cb4d7a0c81d29a150b5c
Nathaniel Jennings <headed2iu@gmail.com>
2019-04-02 14:10:40 -0400

    Update credentials_sample.py

--- credentials_sample.py
+++ credentials_sample.py
@@ -9,14 +9,37 @@ committees_directory = "[COMMITTEES ROOT DIRECTORY]"
 def generateSession() -> requests.Session:
     session = requests.Session()
     session.auth = (username, password)
-    session.get(login_url)
-    return session
 
-def getLoginURL() -> str:
-    return login_url
+    max_retries = 3
+    backoff_increment = 1
+
+    retries = Retry(
+        total=max_retries,
+        read=max_retries,
+        connect=max_retries,
+        backoff_factor=backoff_increment,
+        status_forcelist=(500, 502, 504)
+    )
+
+    adapter = HTTPAdapter(max_retries=retries)
+
+    session.mount("http://", adapter)
+    session.mount("https://", adapter)
+
+    session.verify = False
+
+
+    session.get(login_url)
+
+    return session
 
 def getBaseURL() -> str:
     return base_url
 
 def getCommitteesDirectory() -> str:
+    
+    current_directory = os.path.dirname(os.path.realpath(__file__))
+
+    committees_directory = current_directory + "/minutes_pdfs/"
+    
     return committees_directory
